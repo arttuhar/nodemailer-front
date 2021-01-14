219b12d3be231fe86618893b4fc92cd6736667e5
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-01-14 15:43:20 +0200

    Handlers and functions for submit and reset

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,77 @@
 import React, { Component } from "react";
 import "./Form.css";
+import axios from "axios";
 
 export default class Form extends Component {
+	state = {
+		firstname: "",
+		lastname: "",
+		email: "",
+		message: "",
+		sent: false,
+	};
+
+	handleFirstname = e => {
+		this.setState({
+			firstname: e.target.value,
+		});
+	};
+
+	handleLastname = e => {
+		this.setState({
+			lastname: e.target.value,
+		});
+	};
+
+	handleEmail = e => {
+		this.setState({
+			email: e.target.value,
+		});
+	};
+
+	handleMessage = e => {
+		this.setState({
+			message: e.target.value,
+		});
+	};
+
+	submitMessage = e => {
+		e.preventDefault();
+		let data = {
+			firstname: this.state.firstname,
+			lastname: this.state.lastname,
+			email: this.state.email,
+			message: this.state.message,
+		};
+		axios
+			.post("/api/forma", data)
+			.then(res => {
+				this.setState(
+					{
+						sent: true,
+					},
+					this.resetForm()
+				);
+			})
+			.catch(() => {
+				console.log("Failed to send message");
+			});
+	};
+
+	resetForm = () => {
+		this.setState({
+			firstname: "",
+			lastname: "",
+			email: "",
+			message: "",
+		});
+		setTimeout(() => {
+			this.setState({
+				sent: false,
+			});
+		}, 5000);
+	};
+
 	render() {
 		return (
 			<div className="formcontainer">
@@ -13,6 +83,8 @@ export default class Form extends Component {
 							name="firstname"
 							classname="firstname"
 							placeholder="First name"
+							value={this.state.firstname}
+							onChange={this.handleFirstname}
 						/>
 					</div>
 					<div className="formitem">
@@ -22,6 +94,8 @@ export default class Form extends Component {
 							name="lastname"
 							classname="lastname"
 							placeholder="Last name"
+							value={this.state.lastname}
+							onChange={this.handleLastname}
 						/>
 					</div>
 					<div className="formitem">
@@ -31,6 +105,8 @@ export default class Form extends Component {
 							name="email"
 							classname="email"
 							placeholder="Email"
+							value={this.state.email}
+							onChange={this.handleEmail}
 						/>
 					</div>
 					<div classname="messagearea formitem">
@@ -40,6 +116,8 @@ export default class Form extends Component {
 							cols="30"
 							rows="5"
 							placeholder="Enter your message"
+							value={this.state.message}
+							onchance={this.handleMessage}
 						></textarea>
 					</div>
 					<div className="messagesent">Message has been sent..</div>
